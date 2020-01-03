86c8dc22454fb7cb2c8ca715aef3a9cbf9ec8281
​Snuggle <^-^@Snugg.ie>
2020-01-03 16:11:11 +0000

    Update photoanimate.sh

--- photoanimate.sh
+++ photoanimate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ffmpeg -f image2 -r 1/0.3 -pattern_type glob -i '*.jpg' -c:v libx265 -vf scale="(iw*sar)*min(2560/(iw*sar)\,1440/ih):ih*min(2560/(iw*sar)\,1440/ih), pad=2560:1440:(2560-iw*min(2560/iw\,1440/ih))/2:(1440-ih*min(2560/iw\,1440/ih))/2" out.mp4 # https://superuser.com/questions/249101/how-can-i-combine-30-000-images-into-a-timelapse-movie
+ffmpeg -f image2 -r 1/0.3 -pattern_type glob -i '*.jpg' -c:v libx265 -metadata creation_time="2019-08-06 12:00:00" -vf scale="(iw*sar)*min(2560/(iw*sar)\,1440/ih):ih*min(2560/(iw*sar)\,1440/ih), pad=2560:1440:(2560-iw*min(2560/iw\,1440/ih))/2:(1440-ih*min(2560/iw\,1440/ih))/2" out.mp4 # https://superuser.com/questions/249101/how-can-i-combine-30-000-images-into-a-timelapse-movie
