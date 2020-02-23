6566750a9d10411349db3ed943dbd90e7e0c8533
​Snuggle <^-^@Snugg.ie>
2020-02-23 14:03:57 +0000

    Add files via upload

---
+++ steamDiscoveryQueue.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while :; do
+	xdotool sleep 1
+	xdotool mousemove 1417 346
+	xdotool sleep 1
+	xdotool mousemove 1289 780
+	xdotool sleep 1
+	xdotool click 1289 780
+	xdotool sleep 1
+done
