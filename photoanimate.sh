08cd73d07f7cf5e7f2aa34113795d9ce853dd490
Evie ​Snuggle <^-^@Snugg.ie>
2021-01-12 23:03:57 +0000

    Update photoanimate.sh

--- photoanimate.sh
+++ photoanimate.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-ffmpeg -f image2 -r 1/0.3 -pattern_type glob -i '*.jpg' -c:v libx265 -metadata creation_time="2019-08-06 12:00:00" -vf scale="(iw*sar)*min(2560/(iw*sar)\,1440/ih):ih*min(2560/(iw*sar)\,1440/ih), pad=2560:1440:(2560-iw*min(2560/iw\,1440/ih))/2:(1440-ih*min(2560/iw\,1440/ih))/2" out.mp4 # https://superuser.com/questions/249101/how-can-i-combine-30-000-images-into-a-timelapse-movie
+for f in *.heic;do heif-convert "$f" "${f%heic}jpg";done
+echo "Removing all HEIC images..."
+read -p "Press ENTER to continue."
+rm *.heic
+ffmpeg -f image2 -r 1/0.3 -pattern_type glob -i '*.jpg' -c:v libx264 -metadata creation_time="2019-08-06 12:00:00" -vf scale="(iw*sar)*min(3840/(iw*sar)\,2160/ih):ih*min(3840/(iw*sar)\,2160/ih), pad=3840:2160:(3840-iw*min(3840/iw\,2160/ih))/2:(1440-ih*min(3840/iw\,2160/ih))/2" out.mp4 # https://superuser.com/questions/249101/how-can-i-combine-30-000-images-into-a-timelapse-movie
